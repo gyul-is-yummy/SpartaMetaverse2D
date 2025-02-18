dfbcb7c1039496e937994645dbcdc079a89b9f28
gyul-is-yummy <gyulisyummy@gmail.com>
2025-02-19 00:44:36 +0900

    ✨Feat: 카메라 기능 구현
    FollowCamera 스크립트를 만들어 카메라가 캐릭터를 따라 움직이게 만듦.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,12 +31,9 @@ public class PlayerController : BaseController
         //아래(혹은 S)키를 누르면 -1, 위(혹은 W)키를 누르면 1, 아무것도 안 누르면 0 반환
         float vertical = Input.GetAxisRaw("Vertical");
 
-        
-
         //반환된 숫자를 벡터 값으로 사용한다.
         //normalized == 백터의 크기를 1로 만들어 방향만을 추출
         movementDirection = new Vector2(horizontal, vertical).normalized;
         //Debug.Log(movementDirection);
-
     }
 }
